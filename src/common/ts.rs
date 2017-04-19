157e6c6c854efe9ddca52c4818217f0e4845002e
panicbit <panicbit.dev@gmail.com>
2017-04-19 12:15:20 +0200

    Impl a few common traits for common::Ts

--- src/common/ts.rs
+++ src/common/ts.rs
@@ -2,6 +2,7 @@ use ffi::{AVRational, av_compare_ts};
 use std::cmp;
 use std::ops;
 
+#[derive(Copy,Clone)]
 pub struct Ts {
     index: i64,
     time_base: AVRational,
@@ -32,16 +33,24 @@ impl cmp::PartialEq for Ts {
     }
 }
 
+impl cmp::Eq for Ts {}
+
 impl cmp::PartialOrd for Ts {
     fn partial_cmp(&self, other: &Ts) -> Option<cmp::Ordering> {
+        Some(self.cmp(other))
+    }
+}
+
+impl cmp::Ord for Ts {
+    fn cmp(&self, other: &Ts) -> cmp::Ordering {
         unsafe {
             use std::cmp::Ordering::*;
-            Some(match av_compare_ts(self.index, self.time_base, other.index, other.time_base) {
+            match av_compare_ts(self.index, self.time_base, other.index, other.time_base) {
                 -1 => Less,
                  0 => Equal,
                  1 => Greater,
                  _ => unreachable!("av_compare_ts BUG")
-            })
+            }
         }
     }
 }
